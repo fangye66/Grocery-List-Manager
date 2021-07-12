035985f800e65093e9156ddd5dbbda546b83a19f
fangye66 <fangye.yf@gmail.com>
2021-07-12 00:07:28 -0400

    Update app.grocery.ts

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -1,1 +1,23 @@
 
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './app.grocery.html',
+  styleUrls: [
+    
+      '../assets/css/bootstrap-3.3.7/dist/css/bootstrap.min.css'
+      
+  ]
+})
+
+export class GroceryComponent {
+  task!: string;
+  tasks: {name: string, strike : boolean}[] = [];
+
+  onClick(){
+    this.tasks.push({name: this.task, strike: false});
+    this.task = '';
+  }
+}
